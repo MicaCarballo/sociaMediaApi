7116004e7b223978f286988d40a28bc819c978ce
MicaCarballo <micacarballo2@gmail.com>
2023-01-08 22:42:04 -0300

    changed email

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-        user: 'micacarballo2@gmail.com',
+        user:"micacarballo2@gmail.com",
         pass: config.api.emailPass
     }
 })
