88a77f4dff9ab29ff426b1386db369a73dce5cdf
MicaCarballo <micacarballo2@gmail.com>
2023-03-30 11:43:17 -0300

    changed follow user controller

--- src/follows/follows.controllers.js
+++ src/follows/follows.controllers.js
@@ -3,14 +3,36 @@ const uuid = require('uuid')
 const Follows = require('../models/follows.models')
 const Users = require('../models/users.models')
 
+// const followUser = async (follower, following) => {
+//     const data = await Follows.create({
+//         id: uuid.v4(),
+//         userId: follower,
+//         userId2: following
+//     })
+//     return data
+// }
+
 const followUser = async (follower, following) => {
-    const data = await Follows.create({
+  const follow = await Follows.findOne({
+    where: {
         id: uuid.v4(),
         userId: follower,
         userId2: following
-    })
-    return data
+
+    }
+  })
+  if(!follow){
+    const data = await Follows.create({
+                id: uuid.v4(),
+                userId: follower,
+                userId2: following
+            })
+            return data
+  }
+
 }
+
+   
 
 const findMyFollowers = async (userId) => {
     const data = await Follows.findAll({
